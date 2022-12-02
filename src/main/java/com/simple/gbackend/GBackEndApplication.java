855fcca8b5cf073b7be8d4e288961192fc8feb3e
cumt_gjc <pipigejc@163.com>
2022-12-02 16:04:57 +0800

    Initial commit

---
+++ src/main/java/com/simple/gbackend/GBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.simple.gbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GBackEndApplication.class, args);
+    }
+
+}
